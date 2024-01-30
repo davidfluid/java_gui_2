9fd11d951f97d9b49eba0f186a7226a100efe5d1
David <64472486+davidfluid@users.noreply.github.com>
2024-01-30 10:14:57 +0530

    Create Sql_signin.java

---
+++ sql_signin/sql_signin/src/main/java/com/mycompany/sql_signin/Sql_signin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sql_signin;
+
+/**
+ *
+ * @author JESUS
+ */
+public class Sql_signin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
