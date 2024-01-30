bc756f6c059409badf185bfd27e7ddf6a61fd0de
David <64472486+davidfluid@users.noreply.github.com>
2024-01-30 10:12:47 +0530

    Create Database_demo.java

---
+++ dialog_box/database_demo/src/main/java/com/mycompany/database_demo/Database_demo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.database_demo;
+
+/**
+ *
+ * @author JESUS
+ */
+public class Database_demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
